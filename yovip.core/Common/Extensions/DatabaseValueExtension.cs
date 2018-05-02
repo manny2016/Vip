f8ec7de29c3bdffdb7c6ffef116cc8b49d7ab3b6
Manny <Manny.Qiao@hotmail.com>
2018-05-02 15:15:15 +0800

    [Manny]Add  Database Value Extension class to help convert value.

---
+++ yovip.core/Common/Extensions/DatabaseValueExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoVip.Core
+{
+    public static class DatabaseValueExtension
+    {
+        public static bool IsDBNull(this object obj)
+        {
+            if (obj == null) return false;
+            return obj.GetType().Name.Equals("DBNull");
+        }
+    }
+}
